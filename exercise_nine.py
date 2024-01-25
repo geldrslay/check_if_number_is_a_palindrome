d90d9b7002f4b7fcc673231de7d98a7e69b8bd3d
Angela Ferrer <gelferrero01@gmail.com>
2024-01-26 00:52:08 +0800

    defining function

---
+++ exercise_nine.py
@@ -0,0 +1,7 @@
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is the same after reverse.
+
+# Defining function
+def palindrome (given_number):
+    original_number = given_number
+    print ("The given number is:", given_number)
